a26db60a8e0ae8f3a7012c3a3f94e9798e0abc98
Tyler the Pug <50224819+tylerwhardy@users.noreply.github.com>
2025-07-21 23:23:35 -0400

    Update c2.py

--- server/c2.py
+++ server/c2.py
@@ -494,7 +494,7 @@ def initialise_socket():
     """
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    sock.bind(('127.0.0.1', 5555))
+    sock.bind(('0.0.0.0', 5555))
     sock.listen(5)
     return sock
 
